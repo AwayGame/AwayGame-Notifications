529407a50a24ed5ca9fdd7406a36cf219d7b459c
MichaelFioretti <michael@hijro.com>
2018-05-30 13:31:09 -0400

    Add cron package and finish initial setup

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const CronJob = require('cron').CronJob;
 const express = require('express');
 const app = express();
 const http = require('http')
@@ -6,12 +7,10 @@ const axios = require('axios');
 const config = require('./config');
 
 // Initialize Firebase
-
 const functions = require("firebase-functions")
 const admin = require('firebase-admin');
 
 // Initialize the app and database
-
 admin.initializeApp({
     credential: admin.credential.cert(config.appConfig),
     databaseURL: config.databaseURL
@@ -19,6 +18,12 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
+init()
+
 server.listen(config.port, function() {
     console.log("server live on port " + config.port)
 });
+
+function init() {
+	console.log("watching now")
+}
